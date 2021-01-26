50fa146b239606dc90aa2703b5159c65d32b3ecb
emin2045 <emin.taskiran@ceng.deu.edu.tr>
2021-01-26 12:28:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TPSCharacterSetup
+
+Developed with Unreal Engine 4
---
+++ Source/TPSCharacterSetup/TPSCharacterSetupGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPSCharacterSetupGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSCHARACTERSETUP_API ATPSCharacterSetupGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
